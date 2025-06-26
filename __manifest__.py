2e38c572808129b8a0b0c7af24353560b81ca8a7
Danny-360 <dannikk.dc@gmail.com>
2025-06-26 16:39:33 +0100

    Birthday Notification to employees

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Employee Birthday Notifier",
+    "version": "1.0",
+    "depends": ["hr", "mail"],
+    "author": "You",
+    "category": "Human Resources",
+    "summary": "Send birthday notifications to employees and HR",
+    "data": [
+        "data/birthday_templates.xml",
+        "data/birthday_cron.xml"
+    ],
+    "installable": True,
+    "auto_install": False
+}
