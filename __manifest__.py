4edeab6e830c777d6b1a0f765edafaf9b0de48d7
Danny-360 <dannikk.dc@gmail.com>
2025-08-25 11:59:06 +0100

    Socie Report

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "una_asset_module",
+    'name': "Statement of Changes in Equity",
 
     'summary': "Short (1 phrase/line) summary of the module's purpose",
 
@@ -18,17 +18,19 @@ Long description of module's purpose
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account','account_asset'],
+    'depends': ['base','account','account_reports'],
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
     ],
+
+    'installable': True,
+    'application': False,
+    
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
 }
 
--- models/models.py
+++ models/models.py
@@ -1,23 +1,64 @@
 # -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+from odoo.osv.expression import AND
 
-from odoo import models, fields, api
+# class StatementChangeInEquityReport(models.Model):
+class SocieReportHandler(models.AbstractModel):
+    _name = "socie.report.handler"
+    _inherit = "account.report.custom.handler"
+    _description = "SOCIE Report Handler"
 
-
-
-
-# This add the location table to the asset model in odoo
-class AssetLocation(models.Model):
-    _name = 'asset.location'
-    _description = 'Asset Location'
-
-    name = fields.Char(string='Location Name')
+    filter_date = {'mode': 'range', 'filter': 'this_year'}
 
 
-# This adds the custom fields and then other customized fields.
-class una_asset_module(models.Model): 
-    _inherit = 'account.asset'
+    ####################################################    
+    # Report Definition
+    ####################################################
+    def _get_report_name(self):
+        return _("Statement of Changes in Equity")
 
-    asset_location_id = fields.Many2one('asset.location', string='Asset Location')
-    asset_serial = fields.Char(string='Registration Number')
-    asset_invoice = fields.Many2one('account.move', string='Invoice')
-    asset_supplier = fields.Many2one('res.partner', string='Supplier')
+    def _get_columns_name(self, options):
+        return [
+            {'name': _('Issued Share Capital')},
+            {'name': _('Deposit for Shares')},
+            {'name': _('Assets Revaluation')},
+            {'name': _('Retained Earnings')},
+            {'name': _('Total Equity')},
+        ]
+
+    def _get_lines(self, options, line_id=None):
+        lines = []
+        # Example structure, formulas will come later
+        lines.append(self._build_line(_("Balance at end of the year - 2020")))
+        lines.append(self._build_line(_("Profit for year")))
+        lines.append(self._build_line(_("Balance at end of the year - 2021")))
+        lines.append(self._build_line(_("New Capital issued during the year")))
+        lines.append(self._build_line(_("Loss for year")))
+        lines.append(self._build_line(_("Prior year adjustment to spare parts stock")))
+        lines.append(self._build_line(_("Balance at end of the year - 2022")))
+        lines.append(self._build_line(_("New Capital issued during the year")))
+        lines.append(self._build_line(_("Write back of ED Salary")))
+        lines.append(self._build_line(_("COVID-19 Rebate from FGN")))
+        lines.append(self._build_line(_("Prepaid Lease Rental now recognised")))
+        lines.append(self._build_line(_("Profit for year 2023")))
+        lines.append(self._build_line(_("Balance at end of the year - 2023")))
+        lines.append(self._build_line(_("New Capital issued during the year")))
+        lines.append(self._build_line(_("Stamp Duty paid on new Share Capital")))
+        lines.append(self._build_line(_("Bonus Issue made")))
+        lines.append(self._build_line(_("Revaluation of Aircraft")))
+        lines.append(self._build_line(_("Term Loan Swapped for Equity")))
+        lines.append(self._build_line(_("Additional shares Issued")))
+        lines.append(self._build_line(_("Prior year expenses paid")))
+        lines.append(self._build_line(_("Prior year Provision for ITF and NSITF")))
+        lines.append(self._build_line(_("Tax Audit liability paid")))
+        lines.append(self._build_line(_("Profit for year 2024")))
+        lines.append(self._build_line(_("Balance at end of the year - 2024")))
+        return lines
+
+    def _build_line(self, name):
+        return {
+            'id': name,
+            'name': name,
+            'columns': [{'name': ''}] * 5,  # 5 columns
+            'level': 2,
+        }
